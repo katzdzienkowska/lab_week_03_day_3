3cb46ea1de7d2913eb1df7687923e48033d38ad5
Kat Zdzienkowska <katzdzienkowska@kats-mbp.home>
2022-06-01 19:59:22 +0100

    finished lab

---
+++ models/order.py
@@ -0,0 +1,7 @@
+class Order():
+
+    def __init__(self, date, name, quantity, topping):
+        self.date = date
+        self.name = name
+        self.quantity = quantity
+        self.topping = topping
